6c0903abada84cffabb0802f2afb745d84c2c63e
hanzalarohan123 <hanzalarohan@gmail.com>
2022-03-12 19:16:35 +0500

    Added State in using class component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet';
 import Welcome from './Components/Welcome';
+import IncludinState from './Components/IncludinState';
 function App() {
   return (
     <div className="App">
       
         
        
-       <Greet name="sss" age='12'><p>child prop 1</p> </Greet>
+       {/* <Greet name="sss" age='12'><p>child prop 1</p> </Greet>
        <Greet name="sss" age='13'><button>cick me</button></Greet>
        <Greet name="sss" age='14'><p>child prop 3</p></Greet>
        <Welcome name="sss" age='12'/> 
        <Welcome name="sss" age='13'/> 
-       <Welcome name="sss" age='14'/>
+       <Welcome name="sss" age='14'/> */}
+
+       <IncludinState />
+
+
+
     </div>
     
   );
---
+++ src/Components/IncludinState.js
@@ -0,0 +1,39 @@
+import React,{Component} from "react";
+
+class IncludinState extends Component
+
+{
+    //constructor.
+    constructor(props)
+    {
+        //calling super method of base class
+        super(props)
+        this.state =
+        {
+
+            message: "Welcome visitor"
+        }
+    }
+    //should be declared outside constructor because it is not a builtin method
+    changeMessage()
+        {
+            this.setState(
+                {
+                    message: 'Thank you for subscribing!'
+                 } 
+            )
+        }
+        
+    
+
+
+    render()
+    {
+      return ( <div>
+        <h1>{this.state.message}</h1>
+        <button onClick={() => this.changeMessage()}>Subscribe</button>
+      </div>)
+
+    }  
+}
+export default IncludinState
