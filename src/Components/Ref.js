a5d03cab7abc279684531e812a9af85973d3c357
hanzalarohan123 <hanzalarohan@gmail.com>
2022-03-23 03:26:18 +0500

    added react.ref code with one approach and added another usage

--- src/Components/Ref.js
+++ src/Components/Ref.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react'
 
- class Ref extends Component {
-     constructor(props) {
-       super(props)
-     
-       //second step of approach 1
-          this.inputRef=React.createRef()
-       
-     }
-     componentDidMount()
-     {
+class Ref extends Component {
+    constructor(props) {
+        super(props)
+
+        //second step of approach 1
+        this.inputRef = React.createRef()
+
+    }
+    componentDidMount() {
         //third and final step of approach 1 
         this.inputRef.current.focus()
-     }
-  render() {
-    return (
-      <div>
-          {/* first step is to add it step of approach 1 */}
-          <input type='text' ref={this.inputRef} ></input>
-      </div>
-    )
-  }
+    }
+    inputHandler = () => 
+    {
+        //just to pop value entered in ref componnet object
+        alert(this.inputRef.current.value)
+    }
+    render() {
+        return (
+            <div>
+                {/* first step is to add it step of approach 1 */}
+                <input type='text' ref={this.inputRef} ></input>
+                <button onClick={this.inputHandler}>click me</button>
+            </div>
+        )
+    }
 }
 
 export default Ref
