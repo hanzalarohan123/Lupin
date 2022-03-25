0f783f6f2c72db791ce98c6b06291f1897611c8e
hanzalarohan123 <hanzalarohan@gmail.com>
2022-03-26 02:44:15 +0500

    hoc added reusable comp 2

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -2,31 +2,34 @@ import React, { Component } from 'react'
 import updatedComponent from './ReUsableCode'
 
 class Counter extends Component {
-    constructor(props) {
-        super(props)
+    // constructor(props) {
+    //     super(props)
       
-        this.state = {
-           counterx:-1,
-           hoverx:0
-        }
-      }    
-      clickCounter = () =>
-      {
-          this.setState(prevState =>{return {counterx :prevState.counterx + 1}}) 
-      }
-      clickHover = () =>
-      {
-          this.setState(prevState =>{return {hoverx :prevState.hoverx + 1}}) 
-      }
+    //     this.state = {
+    //        counterx:0,
+    //        //hoverx:0
+    //     }
+    //   }    
+    //   clickCounter = () =>
+    //   {
+    //       this.setState(prevState =>{return {counterx :prevState.counterx + 1}}) 
+    //   }
+    //   clickHover = () =>
+    //   {
+    //       this.setState(prevState =>{return {hoverx :prevState.hoverx + 1}}) 
+    //   }
   render() {
       //const {counterx}= this.state
-      let counterx = this.state.counterx
-      let hoverx = this.state.hoverx
+      //let counterx = this.state.counterx
+      //let hoverx = this.state.hoverx
+
+      const {counterx,clickCounter,name}=this.props
     return (
+        
         <div>
 
-<button onClick={this.clickCounter}> {this.props.name} Button is clicked {counterx} Times</button>
-<h1 onMouseOver={this.clickHover}>{this.props.name} hovered {hoverx} Times</h1>
+<button onClick={clickCounter}> {name} Button is clicked {counterx} Times</button>
+{/* <h1 onMouseOver={this.clickHover}>{this.props.name} hovered {hoverx} Times</h1> */}
         </div>
      
     )
--- src/Components/ReUsableCode.js
+++ src/Components/ReUsableCode.js
@@ -3,9 +3,26 @@ import React from "react";
  {
      class NewComponent extends React.Component
      {
+
+//start
+        constructor(props) {
+            super(props)
+          
+            this.state = {
+               counterx:0,
+               //hoverx:0
+            }
+          }    
+          clickCounter = () =>
+          {
+              this.setState(prevState =>{return {counterx :prevState.counterx + 1}}) 
+          }
+//end
+
+
          render()
          {
-             return <OriginalComponent name='lodu'/>
+             return <OriginalComponent name='lodu' counterx={this.state.counterx} clickCounter={this.clickCounter}/>
          }
      }
      return NewComponent
